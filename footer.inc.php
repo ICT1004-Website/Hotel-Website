29cee4dfe32c3e29351a372b93d968ea0abdc362
xianyong98 <71886838+xianyong98@users.noreply.github.com>
2020-11-12 09:17:12 +0800

    Add files via upload

---
+++ footer.inc.php
@@ -0,0 +1,132 @@
+<!--<footer class="container-fluid">
+    
+    <section class="footer1 cid-qJ2WtUFT1h" id="footer1-b">
+    <div class="container">
+        <div class="row justify-content-center mbr-white">
+            <div class="col-md-6 first-column col-lg-4">
+                <h2 class="mbr-section-title mbr-fonts-style pb-2 display-5">
+                    Phone Support
+                </h2>
+                <h3 class="mbr-section-subtitle mbr-fonts-style mbr-lighter display-4">
+                    24 HOURS A DAY
+                </h3>
+                <p class="mbr-text mbr-fonts-style display-5">
+                    + 01 234 567 890
+                </p>
+            </div>
+            <div class="col-md-6 second-column col-lg-4">
+                <h2 class="mbr-section-title mbr-fonts-style pb-2 display-5">
+                    Follow Us
+                </h2>
+                <h3 class="mbr-section-subtitle mbr-fonts-style mbr-lighter display-4">
+                    SOCIAL MEDIA CHANNELS
+                </h3>
+                <div class="icons-wrapper">
+                    <ul>
+                        <li>
+                            
+                        </li>
+                        <li>
+                            <a class="icon-transition" href="https://twitter.com/mobirise">
+                                <span class="mbr-iconfont socicon-twitter socicon"></span>
+                            </a>
+                        </li>
+                        <li>
+                            <a class="icon-transition" href="https://www.youtube.com/c/mobirise">
+                                <span class="mbr-iconfont socicon-youtube socicon"></span>
+                            </a>
+                        </li>
+                        <li>
+                            <a class="icon-transition" href="https://www.instagram.com/mobirise/">
+                                <span class="mbr-iconfont socicon-instagram socicon"></span>
+                            </a>
+                        </li>
+                        <li>
+                            <a class="icon-transition" href="https://www.behance.net/Mobirise">
+                                <span class="mbr-iconfont socicon-behance socicon"></span>
+                            </a>
+                        </li>
+                        
+                    </ul>
+                </div>
+            </div>
+            <div class="col-md-7 col-lg-4 third-column">
+                <h2 class="mbr-section-title mbr-fonts-style pb-2 display-5">
+                    HTML5 Hotel Templates
+                </h2>
+                <h3 class="mbr-section-subtitle mbr-fonts-style mbr-lighter display-4">
+                    SIGN UP FOR SPECIAL OFFERS
+                </h3>
+                <div class="form1" data-form-type="formoid">
+                    <div data-form-alert="" hidden="">
+                        Thanks for filling out the form!
+                    </div>
+                    <form class="mbr-form" action="https://mobirise.com/" method="post" data-form-title="My Mobirise Form"><input type="hidden" name="email" data-form-email="true" value="pyLrCqpRvr+IbGo/9u2U4RCUDIXgYL+raru4eRja3wFNq+ew3xjwD8cLJ6q+YMtcv/i5LmtI5ATOXk/jYeDhkE6uaDsqu2nQcPGaTxV3N6mk28MhpwzWaQDPuMDOPRRK" data-form-field="E-mail">
+                        <div class="input-wrap" data-for="email">
+                            <input type="text" class="field display-7" name="email" data-form-field="E-mail" placeholder="E-mail" required="" id="email-footer1-b">
+                        </div>
+                        <span class="input-group-btn">
+                            <button href="https://mobirise.com/" type="submit" class="btn btn-lg btn-primary display-4">SUBSCRIBE</button>
+                        </span>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+        <p>Copyright &copy; 2020 World of Pets Pte. Ltd.</p>
+</footer>-->
+<footer class="site-footer">
+      <div class="container">
+        <div class="row">
+          <div class="col-sm-12 col-md-6">
+            <h6>About</h6>
+            <p class="text-justify">Scanfcode.com <i>CODE WANTS TO BE SIMPLE </i> is an initiative  to help the upcoming programmers with the code. Scanfcode focuses on providing the most efficient code or snippets as the code wants to be simple. We will help programmers build up concepts in different programming languages that include C, C++, Java, HTML, CSS, Bootstrap, JavaScript, PHP, Android, SQL and Algorithm.</p>
+          </div>
+
+          <div class="col-xs-6 col-md-3">
+            <h6>Categories</h6>
+            <ul class="footer-links">
+              <li><a href="http://scanfcode.com/category/c-language/">C</a></li>
+              <li><a href="http://scanfcode.com/category/front-end-development/">UI Design</a></li>
+              <li><a href="http://scanfcode.com/category/back-end-development/">PHP</a></li>
+              <li><a href="http://scanfcode.com/category/java-programming-language/">Java</a></li>
+              <li><a href="http://scanfcode.com/category/android/">Android</a></li>
+              <li><a href="http://scanfcode.com/category/templates/">Templates</a></li>
+            </ul>
+          </div>
+
+          <div class="col-xs-6 col-md-3">
+            <h6>Quick Links</h6>
+            <ul class="footer-links">
+              <li><a href="http://scanfcode.com/about/">About Us</a></li>
+              <li><a href="http://scanfcode.com/contact/">Contact Us</a></li>
+              <li><a href="http://scanfcode.com/contribute-at-scanfcode/">Contribute</a></li>
+              <li><a href="http://scanfcode.com/privacy-policy/">Privacy Policy</a></li>
+              <li><a href="http://scanfcode.com/sitemap/">Sitemap</a></li>
+            </ul>
+          </div>
+        </div>
+        <hr>
+      </div>
+      <div class="container">
+        <div class="row">
+          <div class="col-md-8 col-sm-6 col-xs-12">
+            <p class="copyright-text">Copyright &copy; 2020 All Rights Reserved by 
+         <a href="#"></a>.
+            </p>
+          </div>
+
+          <div class="col-md-4 col-sm-6 col-xs-12">
+            <ul class="social-icons">
+              <li><a class="facebook" href="#"><i class="fa fa-facebook"></i></a></li>
+              <li><a class="twitter" href="#"><i class="fa fa-twitter"></i></a></li>
+              <li><a class="dribbble" href="#"><i class="fa fa-dribbble"></i></a></li>
+              <li><a class="linkedin" href="#"><i class="fa fa-linkedin"></i></a></li>   
+            </ul>
+          </div>
+        </div>
+      </div>
+</footer>
+
---
+++ nav.inc.php
@@ -0,0 +1,40 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand" href="#">
+        <img alt="Qries" src="https://raw.githubusercontent.com/ICT1004-Website/Hotel-Website/main/logo.png"
+         width=100" height="40">
+    </a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+    <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#dogs">Dogs</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#cats">Cats</a>
+      </li>
+    </ul>
+      <ul class="navbar-nav ml-auto">
+       <li class="nav-item">
+           <a class="nav-link" href="register.php">
+                <span class="material-icons">account_circle</span>
+           </a>
+           
+      </li>
+       <li class="nav-item">
+           <a class="nav-link" href="login.php">
+               <span class="material-icons">login</span>
+           </a>
+      </li>
+      </ul>
+      
+    
+    
+  </div>
+</nav>
+
