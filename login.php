a2086d5a1f38961201329922570ea9bf5352ff7a
KZhongYi <72512832+KZhongYi@users.noreply.github.com>
2020-11-17 19:44:00 +0800

    Update login.php

--- login.php
+++ login.php
@@ -1,9 +1,13 @@
-<!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
+<?php
+session_start();
+
+//If user is logged in, redirect to logout page
+if(isset($_SESSION["memberid"])){
+    header("Location: /logout.php");
+    //Exit to stop content from loading
+    exit;
+}
+?>
 <html>
     <head>
         <?php
@@ -11,28 +15,39 @@ and open the template in the editor.
         ?>
         <title>The Lodge | Sign In</title>
     </head>
+    
     <body>
         <?php
              include "nav.inc.php";
         ?>
+        
         <header class="jumbotron text-center" style="background-color: grey;">
             <h1 class="display-4">Sign In</h1>
             <h6>For existing members</h6>
         </header>
+        
         <main class="container">
             <form action="" method="post" style="margin-left: 25%;margin-right: 25%;">
                 <div class="form-group">
                     <label for="email"><b>Email</b></label>
-                    <input class="form-control" type="email" id="email" required name="email" placeholder="Email">
+                    <?php
+                        //Check for invalid login attempt, place previously entered email if yes
+                        if(isset($_SESSION["email"])) {
+                            echo "<input class=\"form-control\" type=\"email\" id=\"email\" required name=\"email\" placeholder=\"Email\" value=\"".$_SESSION["email"]."\">";
+                        } else {
+                            echo "<input class=\"form-control\" type=\"email\" id=\"email\" required name=\"email\" placeholder=\"Email\">";
+                        }
+                    ?>
                 </div>
                 <div class="form-group">
                     <label for="pwd"><b>Password</b></label>
                     <input class="form-control" type="password" id="pwd" required name="pwd" placeholder="Password">
                 </div>
-                <p>For new members, sign up <a href="#">here</a>.</p>
+                <p>For new members, sign up <a href="/register.php">here</a>.</p>
                 <button class="btn btn-primary" type="submit">Login</button>
             </form>
         </main>
+        
         <br/>
         <?php
              include "footer.inc.php";
