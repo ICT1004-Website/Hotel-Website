0de70e2aa3ae259bba0dae9e5b1020f89bfb8301
Nikoswee <72612257+Nikoswee@users.noreply.github.com>
2020-11-18 16:50:34 +0800

    Add files via upload

---
+++ about.php
@@ -0,0 +1,35 @@
+<html>
+    <head>
+        <?php
+        include "head.inc.php";
+        ?>
+        <title>The Lodge | About Us</title>
+    </head>
+    <body>
+        <?php
+        include "nav.inc.php";
+        ?>
+        <header class="jumbotron text-center" style="background-color: grey;">
+            <h1 class="display-4">The Lodge</h1>
+            <h6>Learn more about us!</h6>
+        </header>
+        <section id="aboutus">
+            <div class="about-section">
+                <div class="inner-container">
+                    <div class="row">
+                        <div class="col-sm-7">
+                            <h2 class="text-center">Our Story</h2>
+                            <p class="storytext"> We are a group of 5 friends who have the same common goal to create our own hotel empire upon our university graduation. 
+                                Our vision is to provide the best experience for our guests. Comfort is our top priority so our guests would not feel unease when staying with us.
+                            </p>
+                        </div>
+                    </div>
+                </div>    
+            </div>
+
+        </section>
+        <?php
+        include "footer.inc.php";
+        ?>
+    </body>
+</html>
