98fe3de77aff8245784eecdbad45e8942781539b
shengyu98 <32284042+shengyu98@users.noreply.github.com>
2020-11-19 10:49:00 +0800

    Update process_register.php

--- process_register.php
+++ process_register.php
@@ -6,7 +6,7 @@ and open the template in the editor.
 -->
 <html>
     <head>
-        <title>The Lodge | Process Login</title>
+        <title>The Lodge | Process Register</title>
         <?php
         include "head.inc.php"
         ?>
@@ -17,110 +17,113 @@ and open the template in the editor.
         ?>
         <main class ="container">
             <?php
-            $email = "";
-            $errorMsg = "";
+            $fname = $email = $pwd_hashed = $lname = $country = $errorMsg = "";
             $success = true;
-
             if ($_SERVER["REQUEST_METHOD"] == "POST") {
+                if (!empty($_POST["fname"])) {
+                    $fname = sanitize_input($_POST["fname"]);
+                }
+
+                if (empty($_POST["lname"])) {
+                    $errorMsg .= "Last name is required.<br>";
+                    $success = false;
+                } else {
+                    $lname = sanitize_input($_POST["lname"]);
+                }
+                if (empty($_POST["country"])) {
+                    $errorMsg .= "Country is required.<br>";
+                    $success = false;
+                } else {
+                    $country = sanitize_input($_POST["country"]);
+                }
                 if (empty($_POST["email"])) {
-                    //The dot equals ".=" is a contatenation equals operator
-                    $errorMsg .= "Email is required. ";
+                    $errorMsg .= "Email is required.<br>";
                     $success = false;
                 } else {
                     $email = sanitize_input($_POST["email"]);
-
-                    //Additional check to make sure e-mail address is well-formed.
+// Additional check to make sure e-mail address is well-formed.
                     if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-                        $errorMsg .= "Invalid email format. ";
+                        $errorMsg .= "Invalid email format.<br>";
                         $success = false;
-                    }
+                    } 
                 }
+                
 
-                if (empty($_POST["pwd"])) {
-                    $errorMsg .= "Password is required. ";
+                if (empty($_POST["pwd"]) || empty($_POST["pwd_confirm"])) {
+                    $errorMsg .= "Password is required.<br>";
                     $success = false;
-            }}
-            
-                else {
-                    $success = false;
-                }
-                if ($success) {
-                    authenticateUser();
-                }
-
-
-                if ($success) {
-                    //Add session variable for member id and email
-                    $_SESSION["memberid"] = $memberid;
-                    $_SESSION["email"] = $_POST["email"];
-                    include "head.inc.php";
-                    echo "<title>The Lodge | Member</title>";
-                    echo "<header class=\"jumbotron text-center\" style=\"background-color: grey;\">";
-                    echo "<h1 class=\"display-4\">" . $fname . " " . $lname . ",<br> welcome back to The Lodge</h1>";
-                    echo "<br><h6>To view our rooms, click <a href=\"/ViewRoom.php\">here</a></h6>";
-                    echo "</header>";
-                    echo "<br><br>";
-                    include "footer.inc.php";
-                    echo "</main>";
-                    echo "</body>";
                 } else {
-                    //Set error messages into session
-                    $_SESSION['error'] = $errorMsg;
-                    $_SESSION['email'] = $email;
-                    //Redirect to login page
-                    header("Location: /project/login.php");
-                    exit;
-                }
 
-                function sanitize_input($data) {
-                    $data = trim($data);
-                    $data = stripslashes($data);
-                    $data = htmlspecialchars($data);
-                    return $data;
-                }
-
-                function authenticateUser() {
-                    global $memberid, $fname, $lname, $email, $pwd_hashed, $errorMsg, $success;
-                    // Create database connection.
-                    $config = parse_ini_file('../../private/db-config.ini');
-                    $conn = new mysqli($config['servername'], $config['username'],
-                            $config['password'], $config['dbname']);
-                    // Check connection
-                    if ($conn->connect_error) {
-                        $errorMsg = "Connection failed: " . $conn->connect_error;
+                    if ($_POST["pwd"] != $_POST["pwd_confirm"]) {
+                        $errorMsg .= "Passwords do not match.<br>";
                         $success = false;
                     } else {
-                        //Enter db name here
-                        $stmt = $conn->prepare("SELECT * FROM the_lodge_member WHERE emailAddress=?");
-                        // Bind & execute the query statement:
-                        $stmt->bind_param("s", $email);
-                        $stmt->execute();
-                        $result = $stmt->get_result();
-                        if ($result->num_rows > 0) {
-                            // Note that email field is unique, so should only have
-                            // one row in the result set.
-                            $row = $result->fetch_assoc();
-                            $memberid = $row["memberId"];
-                            $fname = $row["fname"];
-                            $lname = $row["lname"];
-                            $pwd_hashed = $row["pwd"];
-                            // Check if the password matches:
-                            if (!password_verify($_POST["pwd"], $pwd_hashed)) {
-                                $errorMsg = "Invalid email/password!";
-                                $success = false;
-                            }
-                        } else {
-                            $errorMsg = "Invalid email/password!";
-                            $success = false;
-                        }
-                        $stmt->close();
+                        $pwd_hashed = password_hash($_POST["pwd"], PASSWORD_DEFAULT);
+//                        saveMemberToDB($pwd_hashed);
                     }
-                    $conn->close();
                 }
-                ?>
-            </main>
-            <?php
-            include "footer.inc.php";
+            } else {
+                $success = false;
+            }
+            if ($success){
+                saveMemberToDB();   
+            }
+            
+            if ($success) {
+                
+                echo "<h1>Your registration is successful!</h1>";
+                echo "<h3>Thank you for signing up, $fname $lname.</h3>";
+                echo "<form action='login.php' method='post'>";
+                echo "<div class='form-group'>
+                    <button style='background-color:green' class='btn btn-primary' type='submit'>Log In</button>";
+            } else {
+                echo "<h1>Oops!</h1>";
+                echo "<h3>The following errors were detected:</h3>";
+                echo "<p>" . $errorMsg . "</p>";
+                echo "<form action='register.php' method='post'>";
+                echo "<div class='form-group'>
+                    <button style='background-color:red' class='btn btn-primary' type='submit'>Return to Sign Up</button>";
+            }
+
+//Helper function that checks input for malicious or unwanted content.
+            function sanitize_input($data) {
+                $data = trim($data);
+                $data = stripslashes($data);
+                $data = htmlspecialchars($data);
+                return $data;
+            }
+
+            /*
+             * Helper function to write the member data to the DB
+             */
+
+            function saveMemberToDB() {
+                global $fname, $lname, $email, $country, $pwd_hashed, $errorMsg, $success;
+// Create database connection.
+                $config = parse_ini_file('../../private/db-config.ini');
+                $conn = new mysqli($config['servername'], $config['username'],
+                        $config['password'], $config['dbname']);
+ //Check connection
+                if ($conn->connect_error) {
+                    $errorMsg = "Connection failed: " . $conn->connect_error;
+                    $success = false;
+                } else {
+// Prepare the statement:
+                    $stmt = $conn->prepare("INSERT INTO the_lodge_member (fname, lname, country, pwd, emailAddress) VALUES (?, ?, ?, ?, ?)");
+// Bind & execute the query statement:
+                    $stmt->bind_param("sssss", $fname, $lname, $country, $pwd_hashed, $email);
+                    if (!$stmt->execute()) {
+                        $errorMsg = "Execute failed: (" . $stmt->errno . ") " . $stmt->error;
+                        $success = false;
+                    }
+                    $stmt->close();
+                }
+                $conn->close();
+            }
             ?>
+        </main>
+        <?php
+        include "footer.inc.php";
+        ?>
     </body>
 </html>
