f22ce819f5f8cd7248aefa50ea886579f5410899
zac ser <50769066+Zacser@users.noreply.github.com>
2020-11-23 12:24:31 +0800

    Update booking.php

--- booking.php
+++ booking.php
@@ -8,7 +8,7 @@
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
         <link rel="stylesheet" href="css/booking.css">
         <script defer src="js/booking.js"></script>
-        
+
         <!--payment links-->
         <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.15/jquery.mask.min.js"></script>
@@ -20,102 +20,155 @@
         <?php
         include "nav.inc.php";
         ?>
-        <h1>Booking Confirmation</h1>
+        <h1>Booking Completed</h1>
 
         <div id="svg_wrap"></div>
         <button class="mem_button"  id="login" onclick="window.location.href = 'login.php'", type="button">Member Login</button>
+        <form action="Cbooking.php" method="POST">
         <div class = row>
-        <div class ="multi-steps">
-        <section class="steps">
-            <h3>Room Confirmation</h3>
-            <input type="text" placeholder="Date" />
-            <input type="text" placeholder="No. of rooms and guest" />
-            <input type="text" placeholder="Room Type" />
-            <input type="text" placeholder="Price" />
-        </section>
-
-        <section class="steps">
-            <h3>Special Requests</h3>
-            <input type="text" placeholder="Special Request" />
-        </section>
-
-        <section class="steps">
-            <h3>Guest Information</h3>
-            <div class="row">
-              <div class="split-col">
-                <label class="label">Title:</label>
-                <select name="title" id="title">
-                    <option value="" disabled selected hidden>Prefix:</option>
-                    <option value="Mr.">Mr.</option>
-                    <option value="Mr.">Mr.</option>
-                    <option value="Ms.">Ms.</option>
-                    <option value="Mrs.">Mrs.</option>
-                    <option value="Miss">Miss</option>
-                    <option value="Dr.">Dr.</option>
-                </select>
-              </div>
-              <div class="split-col">
-                <label class="label">First Name:</label>
-                <input type="text" id="fname" name="firstname" placeholder="First Name" required>
-              </div>
-              <div class="split-col">
-                <label class="label">Last Name:</label>
-                <input type="text" required id="lname" name="lastname" required placeholder="Last Name">
-              </div>
+            <div class ="multi-steps">    
+                <section class="steps">
+                    <h3>Room Confirmation</h3>
+                    <input name='special request' type="text" placeholder="Special Request"/>
+                    
+                </section>
+                <?php
+                if (is_null($_SESSION["memberid"])) {
+                    echo "<section class='steps'>";                    
+                    echo "<h3>Guest Information</h3>";
+                    echo "<div class='row'>";
+                    echo "<div class='split-col'>";
+                    echo "<label class='label'>Title:</label>";
+                    echo "<select name='title' id='title'>";
+                    echo "<option value='' disabled selected hidden>Prefix:</option>";
+                    echo "<option value='Mr.'>Mr.</option>";
+                    echo "<option value='Mr.'>Mr.</option>";
+                    echo "<option value='Ms.'>Ms.</option>";
+                    echo "<option value='Mrs.'>Mrs.</option>";
+                    echo "<option value='Miss'>Miss</option>";
+                    echo "<option value='Dr.'>Dr.</option>";
+                    echo "</select>";
+                    echo "</div>";
+                    echo "<div class='split-col'>";
+                    echo "<label class='label'>First Name:</label>";
+                    echo "<input type='text' id='fname' name='firstname' placeholder='First Name' required>";
+                    echo "</div>";
+                    echo "<div class='split-col'>";
+                    echo "<label class='label'>Last Name:</label>";
+                    echo "<input type='text' required id='lname' name='lastname' required placeholder='Last Name'>";
+                    echo "</div>";
+                    echo "</div>";
+                    echo "<label class='label'>Phone Number:</label>";
+                    echo "<input type='text' required id='phone' name='phone' required placeholder='Phone'>";
+                    echo "<label class='label'>Email:</label>";
+                    echo "<input type='text' required class='input' name='email' placeholder='E-mail'>";
+                    echo "</section>";
+                }
+                    
+                else if (isset($_SESSION["memberid"])){
+                    $config = parse_ini_file('../../private/db-config.ini');
+                    $conn = new mysqli($config['servername'], $config['username'],
+                            $config['password'], $config['dbname']);
+                    //Check connection
+                    if ($conn->connect_error) {
+                        $errorMsg = "Connection failed: " . $conn->connect_error;
+                        $success = false;
+                    }
+                    $sql = "SELECT fname, lname, country, emailAddress from the_lodge_member WHERE memberid =".$_SESSION["memberid"]. ";";
+                    $result = $conn->query($sql);
+                    $row = $result->fetch_assoc();
+                    $fname = $row['fname'];
+                    $lname = $row['lname'];
+                    $country = $row['country'];
+                    $email = $row['emailAddress'];
+                    echo "<section class='steps'>";                    
+                    echo "<h3>Member Information</h3>";
+                    echo "<h4> First Name: ".$fname."</h4>";
+                    echo "<h4> Last Name: ".$lname."</h4>";
+                    echo "<h4> Country: ".$country."</h4>";
+                    echo "<h4> Email: ".$email."</h4>";
+                    echo "</section>";
+                    $conn->close();
+                }
+                
+                ?>
+                <section class="steps">
+                    <h3>Payment</h3>                    
+                    <label for='card_holder' class="label">Card Holder Name: <i class="fas fa-user"></i></label>
+                    <input type="text" class="input" name="card_holder" placeholder="Card Holder Name"><br>
+                    <label for='card_number' class="label">Card Number: <i class="far fa-credit-card"></i></label>
+                    <input type="text" required class="input" name="card_number" placeholder="Card Number" data-mask="0000 0000 0000 0000">
+                    <div class="row">
+                        <div class="split-col">
+                            <label class="label">Expiry Date:  <i class="fas fa-calendar-alt"></i></label>
+                            <input type="text" class="input" id="expiry_date" name="expiry_date" placeholder="MM / YY" data-mask="00 / 00">
+                        </div>
+                        <div class="split-col">
+                            <label class="label">CVC:  <i class="fas fa-key"></i></label>
+                            <input type="text" required class="input" id="cvv" name="cvc" placeholder="CVC" data-mask="000">
+                        </div>
+                    </div>
+                </section>
             </div>
-            <div class="row">
-              <div class="split-col">
-                  <input type="tel" id="phone" placeholder="+65">
-              </div>
-              <div class="split-col">
-                <label class="label">Phone Number:</label>
-                <input type="text" required id="phone" name="phone" required placeholder="Phone">
-              </div>
-            </div>        
-            <label class="label">Email:</label>
-            <input type="text" required class="input" name="email" placeholder="E-mail">
-
-        </section>
-
-        <section class="steps">
-            <h3>Confirmation</h3>
-            <input type="text" placeholder="Confirm" />
-        </section>
-        <section class="steps">
-            <h3>Payment</h3>
-            <label class="label">Email:<i class="fas fa-at"></i></label>
-            <input type="text" required class="input" name="email" placeholder="E-mail">
-             <label class="label">Cardholder Name:  <i class="fas fa-user"></i></label>
-            <input type="text" class="input" name="card_holder" placeholder="Card Holder">
-             <label class="label">Card Number:<i class="far fa-credit-card"></i></label>
-            <input type="text" required class="input" name="card_number" placeholder="Card Number" data-mask="0000 0000 0000 0000">
-            <div class="row">
-              <div class="split-col">
-                <label class="label">Expiry Date:<i class="fas fa-calendar-alt"></i></label>
-                <input type="text" class="input" id="expiry_date" name="expiry_date" placeholder="MM / YY" data-mask="00 / 00">
-              </div>
-              <div class="split-col">
-                <label class="label">CVC:<i class="fas fa-key"></i></label>
-                <input type="text" required class="input" id="cvv" name="cvc" placeholder="CVC" data-mask="000">
-              </div>
+            <div class="summary">
+                <div class="container">
+                    <?php
+                    $start = $_POST['start'];
+                    $end = $_POST['end'];
+                    $quantity = $_POST['quantity'];
+                    $roomtype = $_POST['roomtype'];
+                    $price = $_POST['price'];
+                    if ($roomtype == "single room") {
+                        echo "<h3>" . $roomtype . "<span class='summary' style='color:black'><i class='summary'></i></span></h3>";
+                        echo "<p><a href='images/single_room.jpg'>
+                              <img src='images/single_room.jpg' alt='singleroom' title='singleroom' width='300' height='200'></a></p>";
+                        echo "<h5>Arrival Date: " . $start . "</h5>";
+                        echo "<h5>Checkout Date: " . $end . "</h5>";
+                        echo "<h5>No. of Guests: " . $quantity . "</h5>";
+                        echo "<h5>Price: $" . $price . "</h5>";
+                    } else if ($roomtype == "double room") {
+                        echo "<h3>" . $roomtype . "<span class='summary' style='color:black'><i class='summary'></i></span></h3>";
+                        echo "<p><a href='images/double_room.jpg'>
+                              <img src='images/double_room.jpg' alt='double_room' title='double_room' width='300' height='200'></a></p>";
+                        echo "<h5>Arrival Date: " . $start . "</h5>";
+                        echo "<h5>Checkout Date: " . $end . "</h5>";
+                        echo "<h5>No. of Guests: " . $quantity . "</h5>";
+                        echo "<h5>Price: $" . $price . "</h5>";
+                    } else if ($roomtype == "suites") {
+                        echo "<h3>" . $roomtype . "<span class='summary' style='color:black'><i class='summary'></i></span></h3>";
+                        echo "<p><a href='images/suites.jpg'>
+                              <img src='images/suites.jpg' alt='suites_room' title='suites_room' width='300' height='200'></a></p>";
+                        echo "<h5>Arrival Date: " . $start . "</h5>";
+                        echo "<h5>Checkout Date: " . $end . "</h5>";
+                        echo "<h5>No. of Guests: " . $quantity . "</h5>";
+                        echo "<h5>Price: $" . $price . "</h5>";
+                    } else if ($roomtype == "studio") {
+                        echo "<h3>" . $roomtype . "<span class='summary' style='color:black'><i class='summary'></i></span></h3>";
+                        echo "<p><a href='images/studio.jpeg'>
+                              <img src='images/studio.jpeg' alt='studio' title='studio' width='300' height='200'></a></p>";
+                        echo "<h5>Arrival Date: " . $start . "</h5>";
+                        echo "<h5>Checkout Date: " . $end . "</h5>";
+                        echo "<h5>No. of Guests: " . $quantity . "</h5>";
+                        echo "<h5>Price: $" . $price . "</h5>";
+                    } else if ($roomtype == "villa") {
+                        echo "<h3>" . $roomtype . "<span class='summary' style='color:black'><i class='summary'></i></span></h3>";
+                        echo "<p><a href='images/villa.jpg'>
+                              <img src='images/villa.jpg' alt='villa' title='villa' width='300' height='200'></a></p>";
+                        echo "<h5>Arrival Date: " . $start . "</h5>";
+                        echo "<h5>Checkout Date: " . $end . "</h5>";
+                        echo "<h5>No. of Guests: " . $quantity . "</h5>";
+                        echo "<h5>Price: $" . $price . "</h5>";
+                    }
+                    ?>                    
+                </div>
             </div>
         </div>
-        <div class="summary">
-            <div class="container">
-                <h4>Summary <span class="summary" style="color:black"><i class="summary"></i></span></h4>
-                <p><a href="images/singleroom.jpg">
-                <img src="images/singleroom.jpg" alt="singleroom" title="singleroom" width="300" height="200"></a> 
-                <p><a>Single Room</a><span class="price">$15</span></p>
-                <p><a href="#">Product 2</a> <span class="price">$5</span></p>
-                <p><a href="#">Product 3</a> <span class="price">$8</span></p>
-                <p><a href="#">Product 4</a> <span class="price">$2</span></p>
-                <p>Total <span class="price" style="color:black"><b>$30</b></span></p>
-            </div>
+        <div class="button" id="prev">&larr; PREVIOUS</div>
+        <div class="button" id="next">NEXT &rarr;</div>
+        <div>
+        <input class="btn btn-primary" type="submit" value="Confirm Booking" id="submit">
         </div>
-        </div>
-        <div class="button" id="prev">&larr; Previous</div>
-        <div class="button" id="next">Next &rarr;</div>
-        <div class="button" id="submit">Agree and send application</div>
+        </form>
         <?php
         include "footer.inc.php";
         ?>
