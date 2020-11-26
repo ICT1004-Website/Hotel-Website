e1f90b08f2f733c49edcfc85ed031d3981836144
xianyong98 <71886838+xianyong98@users.noreply.github.com>
2020-11-26 09:44:56 +0800

    Update main.js

--- main.js
+++ main.js
@@ -26,3 +26,32 @@ function checkPassword() {
     }
 }
 
+$(document).ready( function() {
+    var now = new Date();
+    var month = (now.getMonth() + 1);               
+    var day = now.getDate();
+    if (month < 10) 
+        month = "0" + month;
+    if (day < 10) 
+        day = "0" + day;
+    var today = now.getFullYear() + '-' + month + '-' + day;
+    $('#arrival_update').val;
+    $('#checkout_update').val;
+});
+
+//Set minimum start date to today//
+var today = new Date();
+var dd = today.getDate();
+var mm = today.getMonth()+1; //January is 0 so need to add 1
+var yyyy = today.getFullYear();
+if(dd<10){
+    dd='0'+dd;
+} 
+if(mm<10){
+    mm='0'+mm;
+} 
+
+today = yyyy+'-'+mm+'-'+dd;
+document.getElementById("arrival_update").setAttribute("min", today);
+document.getElementById("checkout_update").setAttribute("min", today);
+
